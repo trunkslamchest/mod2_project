898d1583c1d1bc446a335078f290abcad5fc7c5e
trunkslamchest <trunk9slamchest@gmail.com>
2020-01-20 14:25:38 -0500

    added favicon/index logo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  config.relative_url_root = ""
+
   # unless Rails.application.config.consider_all_requests_local
   #  rescue_from Exception, with: :record_not_found
   #  rescue_from ActionController::RoutingError, with: :record_not_found
